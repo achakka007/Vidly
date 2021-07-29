9bd618f1392a1ef4ae4fa007c3f034b0753b683c
achakka007 <51894682+achakka007@users.noreply.github.com>
2021-07-29 16:44:42 -0700

    Fixing mongoDB transport2

--- startup/logging.js
+++ startup/logging.js
@@ -1,10 +1,12 @@
 const winston = require('winston');
 require('express-async-errors') // In case async functions have errors.
 require('winston-mongodb');
+const config = require('config');
 
 module.exports = function() {
+    const db = config.get('db');
     winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-    winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost/vidly',level: 'info', tryReconnect: true}));
+    winston.add(new winston.transports.MongoDB({ db: db, level: 'info', tryReconnect: true}));
     winston.add(new winston.transports.Console({ colorize: true, prettyPrint: true}));
 
     winston.exceptions.handle(new winston.transports.File({ filename: 'logfile.log' }));
