e12a3b28f7ace76bae358ffa0e6a45f93066712a
achakka007 <51894682+achakka007@users.noreply.github.com>
2021-07-29 20:53:54 -0700

    2

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 module.exports = function() {
     const db = config.get('db');
-    // console.log('db connection str='+db)
-    // mongoose.connect(db, { useNewUrlParser: true.valueOf, useUnifiedTopology: true})
-    // .then(() => winston.info(`Connected to ${db}`))
+    console.log('db connection str='+db)
+    mongoose.connect(db, { useNewUrlParser: true.valueOf, useUnifiedTopology: true})
+    .then(() => winston.info(`Connected to ${db}`))
 }
