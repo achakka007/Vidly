a527cd14be3d3c17f4c441baa53b04bccf09b5ee
achakka007 <51894682+achakka007@users.noreply.github.com>
2021-07-29 17:12:47 -0700

    Removed mongoDB transport

--- startup/logging.js
+++ startup/logging.js
@@ -1,14 +1,14 @@
 const winston = require('winston');
 require('express-async-errors') // In case async functions have errors.
-require('winston-mongodb');
-const config = require('config');
+// require('winston-mongodb');
+// const config = require('config');
 
 // Changed db
 module.exports = function() {
-    const db = config.get('db');
+    // const db = config.get('db');
     winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-    winston.add(new winston.transports.MongoDB({ db: db, level: 'info', tryReconnect: true
-}));
+//     winston.add(new winston.transports.MongoDB({ db: db, level: 'info', tryReconnect: true
+// }));
     winston.add(new winston.transports.Console({ colorize: true, prettyPrint: true}));
 
     winston.exceptions.handle(new winston.transports.File({ filename: 'logfile.log' }));
