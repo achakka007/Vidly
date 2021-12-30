b65c6ec4dce215dbcf27f96a29850e19bdcbd398
achakka007 <51894682+achakka007@users.noreply.github.com>
2021-12-29 21:15:17 -0800

    Fix

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var cors = require('cors')
 app.use(cors())
 
 require('./startup/logging')();
+console.log('1');
 require('./startup/config')();
+console.log('2');
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/validation')();
@@ -16,5 +18,4 @@ require('./startup/prod')(app);
 // PORT
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => { winston.info(`listening on port ${port}...`); });
-
 module.exports = server;
--- readme2.txt
+++ readme2.txt
@@ -2,7 +2,8 @@ brew services start mongodb-community@4.4
 brew services stop mongodb-community@4.4
 
 export vidly_jwtPrivateKey=secure
-NODE_ENV=test nodemon index.js
+NODE_ENV=test 
+nodemon index.js
 
 cd Documents/CollegeStuff/Classes/NodeTutorial/vidly/
 
