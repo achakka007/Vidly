7aaa357241892e88eb5788277939fbe10b56bb7e
achakka007 <51894682+achakka007@users.noreply.github.com>
2021-12-29 20:49:50 -0800

    minor fixes

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 module.exports = function () {
     // console.log(process.env);
-    const db = process.env.vidly_db ? process.env.vidly_db : config.get('db');
+    const db = config.get('db');
     console.log('db connection str=' + db)
     mongoose.connect(db, { useNewUrlParser: true.valueOf, useUnifiedTopology: true })
         .then(() => winston.info(`Connected to ${db}`))
