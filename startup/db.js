62dce1c2f8dda938f0ba31f60b23baa063a66dcb
achakka007 <51894682+achakka007@users.noreply.github.com>
2021-12-29 19:54:59 -0800

    test

--- startup/db.js
+++ startup/db.js
@@ -2,9 +2,10 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
-module.exports = function() {
-    const db = config.get('db');
-    console.log('db connection str='+db)
-    mongoose.connect(db, { useNewUrlParser: true.valueOf, useUnifiedTopology: true})
-    .then(() => winston.info(`Connected to ${db}`))
+module.exports = function () {
+    // console.log(process.env);
+    const db = process.env.vidly_db ? process.env.vidly_db : config.get('db');
+    console.log('db connection str=' + db)
+    mongoose.connect(db, { useNewUrlParser: true.valueOf, useUnifiedTopology: true })
+        .then(() => winston.info(`Connected to ${db}`))
 }
